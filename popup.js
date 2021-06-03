433ebb569ef628bcf2ff8bf581f341aa37684729
Wenjie <wenjietj@gmail.com>
2021-06-04 00:43:45 +0800

    popup displays title

--- popup.js
+++ popup.js
@@ -1,28 +1,38 @@
-// set current tab url
-chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-	// get url
-	let url = tabs[0].url;
-	// set url on popup
-	document.querySelector('.url').innerHTML = url;
+const initPageData = () => {
+	// set current tab url
+	chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+		// get url from tabs
+		let url = tabs[0].url;
 
-	// set select option for article type
-	if (url.includes('medium')) {
-		document.getElementById('article_type').value = 'medium';
-	} else if (url.includes('dev.to')) {
-		document.getElementById('article_type').value = 'dev.to';
-	} else {
-		document.getElementById('article_type').value = 'webpage';
-	}
-});
+		// set select option for article type
+		if (url.includes('medium')) {
+			document.getElementById('article_type').value = 'medium';
+		} else if (url.includes('dev.to')) {
+			document.getElementById('article_type').value = 'dev.to';
+		} else {
+			document.getElementById('article_type').value = 'webpage';
+		}
+
+		// set title
+		document.querySelector('.title').innerHTML = tabs[0].title;
+		// set url on popup
+		document.querySelector('.url').innerHTML = url;
+	});
+};
+
+const setTitle = () => {
+	// set title from content
+	chrome.runtime.onMessage.addListener((message) => {
+		alert(message.title);
+	});
+};
 
 const onClick = () => {
-	let url = document.querySelector('.url').textContent;
-
-	// set form data
+	// set data payload
 	const data = {
-		url: url,
 		user: '3',
-		title: '',
+		url: document.querySelector('.url').textContent,
+		title: document.querySelector('.title').textContent,
 		article_type: document.getElementById('article_type').value,
 		description: document.getElementById('description').value,
 		tags: document.getElementById('tags').value,
@@ -32,8 +42,14 @@ const onClick = () => {
 };
 
 // event listeners
-document.addEventListener('DOMContentLoaded', () => {
-	document
-		.querySelector('.button-primary')
-		.addEventListener('click', onClick);
-});
+document.addEventListener(
+	'DOMContentLoaded',
+	() => {
+		document
+			.querySelector('.button-primary')
+			.addEventListener('click', onClick);
+		initPageData();
+		setTitle();
+	},
+	false
+);
