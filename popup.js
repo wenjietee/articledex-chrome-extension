f4ca6587a92bb44880721a931ca5ceac559348a3
Wenjie <wenjietj@gmail.com>
2021-06-03 22:14:18 +0800

    able to get tabs url and article type

--- popup.js
+++ popup.js
@@ -1,13 +1,30 @@
+// set current tab url
+chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+	document.querySelector('.url').innerHTML = tabs[0].url;
+});
+
 const onClick = () => {
-    // set form data
+	let url = document.querySelector('.url').textContent;
+	let article_type = '';
+
+	// check for article type
+	if (url.includes('medium')) {
+		article_type = 'medium';
+	} else if (url.includes('dev.to')) {
+		article_type = 'dev.to';
+	} else {
+		article_type = 'webpage';
+	}
+
+	// set form data
 	const data = {
-		url: 'www.test.com',
-		user: 'username',
-		title: document.getElementById('title').value,
-		article_type: document.getElementById('article_type').value,
+		url: url,
+		article_type: article_type,
+		user: '3',
 		description: document.getElementById('description').value,
 		tags: document.getElementById('tags').value,
 	};
+
 	alert(JSON.stringify(data, null, 2));
 };
 
